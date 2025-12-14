ae59c8a2cb15d4025ff50fa83bdb15f465ee2dd3
gajbhiyepiyush9363 <gajbhiyepiyush9363@gmail.com>
2025-12-14 16:28:23 +0530

    Remove Terraform provider configuration

--- terraform.tf
+++ terraform.tf
@@ -1,14 +1,1 @@
 # terraform block
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.26.0"
-    }
-  }
-}
-
-# provider block
-provider "aws" {
-  region = "ap-south-1"
-}
